5abd46a6bb9bb4233dc86e14d87d0eeb863464ec
DamianJ <dajar80@gmail.com>
2022-08-07 22:31:58 -0300

    mas pruebas formulario

--- PHP/form.php
+++ PHP/form.php
@@ -2,20 +2,22 @@
 
 $nombre = $_POST["nombre"];
 $tel = $_POST["tel"];
-$mail = $_POST["mail"]
-$mensaje = $_POST["mensaje"]
+$mail = $_POST["mail"];
+$mensajes = $_POST["mensaje"];
+
+/* $header .= "Content-Type: text/plain"; */
 
 /* Como me va a llegar el mensaje */
-$mensaje = "Este mensaje fue enviado por" . $nombre . ",\r\n";
-$mensaje .= "Su teléfono es": . $tel . ",\r\n";
+$mensaje = "Este mensaje fue enviado por:" . $nombre . ",\r\n";
+$mensaje .= "Su teléfono es:" . $tel . ",\r\n";
 $mensaje .= "Se mail es:" . $mail . ",\r\n"; 
-$mensaje .= "Mensaje:" . $mensaje . $_POST["mensaje"] .  ",\r\n";
+$mensaje .= "Mensaje:" . $mensajes . $_POST["mensaje"] .  ",\r\n";
 $mensaje .= "Enviado el" . date("d/m/Y", time());
 
-$destinatario = "dajar80@gmail.com";
-$asunto = "Este mail fue enviado desde Domos Fym"
+$para = "dajar80@gmail.com";
+$asunto = "Este mail fue enviado desde Domos Fym";
 
 /* A quien le mando el mail */
-mail($destinatario, $asunto, utf8_decode($mensaje), $header);
+mail($para, $asunto, utf8_decode($mensaje), $header);
 /* Redirección al tocar enviar */
 header("Location:../paginas/enviado.html");
